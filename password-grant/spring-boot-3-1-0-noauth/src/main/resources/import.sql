f0e65523c100ba211b89da2b228ca17b09394221
Lucass-Alvess <lucas_oliveira-01@outlook.com>
2025-09-08 19:39:23 -0300

    BCrypt password

--- password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$qXqDWQyzh/OhXA4XKp3vZuYiB4ITGCS8uhg4Bdjc1hGw5MuYI69mO');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$qXqDWQyzh/OhXA4XKp3vZuYiB4ITGCS8uhg4Bdjc1hGw5MuYI69mO');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
